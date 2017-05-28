30fe2db2c60e9b1065dfae97a225a24d137b5acd
corbak74 <r.venant.valery@gmail.com>
2017-05-28 15:32:49 +0200

    First commit...

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Restart docker & stop existing  services
+# sudo service apache2 restart
+# sudo service mysql restart
+sudo service docker restart
+# Delete all containers
+sudo docker rm $(sudo docker ps -a -q)
+# Delete all images
+sudo docker rmi $(sudo docker images -q)
+
+sudo docker-compose build &
+sudo docker-compose up &
+
+sudo docker run -v /var/www/sql:/sql --link dockertests_database_1:mysql -it arey/mysql-client -h mysql -ppassword -D mysite -e "source /sql/export.sql" &
+
+cd atom
+
+sudo docker build -t test/rvv .
+sudo xhost +
+sudo docker run -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY test/rvv &
+
